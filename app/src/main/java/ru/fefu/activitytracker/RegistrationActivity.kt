2faa1138e9f3ef37a63e1be698226377f419dbc4
Evgeny Shchukin <etozhezhenechka@outlook.com>
2021-10-06 21:23:31 +1000

    Finish registration layout

--- app/src/main/java/ru/fefu/activitytracker/RegistrationActivity.kt
+++ app/src/main/java/ru/fefu/activitytracker/RegistrationActivity.kt
@@ -1,5 +1,29 @@
 package ru.fefu.activitytracker
 
+import android.os.Bundle
+import android.text.method.LinkMovementMethod
+import android.widget.ArrayAdapter
+import android.widget.ImageButton
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.textfield.MaterialAutoCompleteTextView
+import com.google.android.material.textfield.TextInputLayout
 
-class RegistrationActivity : AppCompatActivity(R.layout.activity_registration)
+class RegistrationActivity : AppCompatActivity(R.layout.activity_registration) {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        val backBtn = findViewById<ImageButton>(R.id.back_reg_btn)
+        backBtn.setOnClickListener {
+            finish()
+        }
+
+        val sexList = listOf(getString(R.string.sex_male), getString(R.string.sex_female))
+        val sexInput = findViewById<TextInputLayout>(R.id.sex_reg_input)
+        val adapter = ArrayAdapter(baseContext, android.R.layout.simple_list_item_1, sexList)
+        (sexInput.editText as? MaterialAutoCompleteTextView)?.setAdapter(adapter)
+
+        val privacyText = findViewById<TextView>(R.id.policy_reg_text)
+        privacyText.movementMethod = LinkMovementMethod.getInstance()
+    }
+}
