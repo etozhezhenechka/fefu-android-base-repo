c769cb89d4c9f6ac79c47bb3315098fb5c4762ee
Evgeny Shchukin <etozhezhenechka@outlook.com>
2021-10-13 16:54:32 +1000

    Implement basic methods of fragments, view binding

---
+++ app/src/main/java/ru/fefu/activitytracker/tracker/ActivityFragment.kt
@@ -0,0 +1,38 @@
+package ru.fefu.activitytracker.tracker
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import ru.fefu.activitytracker.R
+import ru.fefu.activitytracker.databinding.FragmentActivityBinding
+
+class ActivityFragment : Fragment(R.layout.fragment_activity) {
+    private var _binding: FragmentActivityBinding? = null
+    private val binding get() = _binding!!
+
+    companion object {
+        const val tag = "activity_fragment"
+
+        fun newInstance() : ActivityFragment {
+            val fragment = ActivityFragment()
+            fragment.arguments = Bundle()
+            return fragment
+        }
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentActivityBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
---
+++ app/src/main/java/ru/fefu/activitytracker/tracker/ProfileFragment.kt
@@ -0,0 +1,38 @@
+package ru.fefu.activitytracker.tracker
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import ru.fefu.activitytracker.R
+import ru.fefu.activitytracker.databinding.FragmentProfileBinding
+
+class ProfileFragment : Fragment(R.layout.fragment_profile) {
+    private var _binding: FragmentProfileBinding? = null
+    private val binding get() = _binding!!
+
+    companion object {
+        const val tag = "activity_fragment"
+
+        fun newInstance() : ActivityFragment {
+            val fragment = ActivityFragment()
+            fragment.arguments = Bundle()
+            return fragment
+        }
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentProfileBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
