74d5a89cf183edef162b13148d3125e153d0cf95
Evgeny Shchukin <etozhezhenechka@outlook.com>
2021-10-27 22:52:18 +1000

    Add classes for data

---
+++ app/src/main/java/ru/fefu/activitytracker/tracker/model/ActivityInfo.kt
@@ -0,0 +1,38 @@
+package ru.fefu.activitytracker.tracker.model
+
+import android.os.Build
+import androidx.annotation.RequiresApi
+import java.time.Duration
+import java.time.LocalTime
+import kotlin.math.abs
+
+class ActivityInfo(private val distance: Double, private val startTime: LocalTime,
+                   private val endTime: LocalTime, private val type: String,
+                   private val username: String = "") {
+
+    private fun getDurationString(duration: Duration, hoursOnly: Boolean = false): String {
+        val hours = duration.toHours()
+        val minutes = (duration.seconds % (60 * 60) / 60).toInt()
+
+        return if (!hoursOnly) "$hours ч. $minutes м."
+        else "$hours ч."
+    }
+
+    fun getDistance(): String = "$distance км."
+
+    fun getDuration(): String {
+        val duration = Duration.between(endTime, startTime).abs()
+        return getDurationString(duration)
+    }
+
+    fun getOffset(): String {
+        val currentDate = LocalTime.now()
+
+        val offset = Duration.between(currentDate, endTime).abs()
+        return "${getDurationString(offset, true)} назад"
+    }
+
+    fun getType(): String = type
+
+    fun getUsername(): String = username
+}
