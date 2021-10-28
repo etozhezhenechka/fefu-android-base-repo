3855f2f5240dd6450a88bdce037de2ef34ce6bea
Evgeny Shchukin <etozhezhenechka@outlook.com>
2021-10-28 12:52:47 +1000

    Add a code to test cards

--- app/src/main/java/ru/fefu/activitytracker/tracker/fragment/MyActivityFragment.kt
+++ app/src/main/java/ru/fefu/activitytracker/tracker/fragment/MyActivityFragment.kt
@@ -5,12 +5,17 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
 import ru.fefu.activitytracker.R
 import ru.fefu.activitytracker.databinding.FragmentMyActivityBinding
+import ru.fefu.activitytracker.tracker.adapter.MyActivityAdapter
+import ru.fefu.activitytracker.tracker.model.*
+import java.time.LocalDateTime
 
 class MyActivityFragment : Fragment(R.layout.fragment_my_activity) {
     private var _binding: FragmentMyActivityBinding? = null
     private val binding get() = _binding!!
+    private lateinit var items: MutableList<CardItemModel>
 
     companion object {
         const val tag = "my_activity_fragment"
@@ -29,6 +34,39 @@ class MyActivityFragment : Fragment(R.layout.fragment_my_activity) {
     ): View {
         _binding = FragmentMyActivityBinding.inflate(inflater, container, false)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val recycleView = binding.myActivityRecyclerView
+        recycleView.layoutManager = LinearLayoutManager(activity)
+
+        val activitiesList = listOf(
+            ActivityInfo(
+                11.4,
+                LocalDateTime.of(2021, 10, 27, 11, 22),
+                LocalDateTime.of(2021, 10, 27, 12, 40),
+                "Серфинг"
+            ),
+
+            ActivityInfo(
+                14.8,
+                LocalDateTime.of(2021, 10, 27, 7, 40),
+                LocalDateTime.of(2021, 10, 27, 10, 59),
+                "Велосипед"
+            )
+        )
+
+        items = mutableListOf(
+            DateLabelModel(FormattedDate(27, 10, 2021).toString())
+        )
+
+        for (item in activitiesList) {
+            items.add(ActivityModel(item))
+        }
+
+        recycleView.adapter = MyActivityAdapter(items)
     }
 
     override fun onDestroyView() {
--- app/src/main/java/ru/fefu/activitytracker/tracker/fragment/UsersActivityFragment.kt
+++ app/src/main/java/ru/fefu/activitytracker/tracker/fragment/UsersActivityFragment.kt
@@ -5,12 +5,17 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
 import ru.fefu.activitytracker.R
 import ru.fefu.activitytracker.databinding.FragmentUsersActivityBinding
+import ru.fefu.activitytracker.tracker.adapter.UsersActivityAdapter
+import ru.fefu.activitytracker.tracker.model.*
+import java.time.LocalDateTime
 
 class UsersActivityFragment : Fragment(R.layout.fragment_users_activity) {
     private var _binding: FragmentUsersActivityBinding? = null
     private val binding get() = _binding!!
+    private lateinit var items: MutableList<CardItemModel>
 
     companion object {
         const val tag = "users_activity_fragment"
@@ -29,6 +34,39 @@ class UsersActivityFragment : Fragment(R.layout.fragment_users_activity) {
     ): View {
         _binding = FragmentUsersActivityBinding.inflate(inflater, container, false)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val recycleView = binding.userActivityRecyclerView
+        recycleView.layoutManager = LinearLayoutManager(activity)
+
+        val activitiesList = listOf(
+            ActivityInfo(
+                11.4,
+                LocalDateTime.of(2021, 9, 27, 11, 22),
+                LocalDateTime.of(2021, 9, 27, 12, 40),
+                "Серфинг", "@etozhezhenechka"
+            ),
+
+            ActivityInfo(
+                14.8,
+                LocalDateTime.of(2021, 9, 26, 7, 40),
+                LocalDateTime.of(2021, 9, 26, 10, 59),
+                "Велосипед", "@etozhezhenechka_second"
+            )
+        )
+
+        items = mutableListOf(
+            DateLabelModel(FormattedDate(27, 9, 2021).toString())
+        )
+
+        for (item in activitiesList) {
+            items.add(UsersActivityModel(item))
+        }
+
+        recycleView.adapter = UsersActivityAdapter(items)
     }
 
     override fun onDestroyView() {
