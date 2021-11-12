47d3a368efa300b323b215c35609f65d9e698e32
Evgeny Shchukin <etozhezhenechka@outlook.com>
2021-11-12 19:02:16 +1000

    Add Insert when you click the "Start" button

--- app/src/main/java/ru/fefu/activitytracker/database/ActivityDatabase.kt
+++ app/src/main/java/ru/fefu/activitytracker/database/ActivityDatabase.kt
@@ -2,7 +2,9 @@ package ru.fefu.activitytracker.database
 
 import androidx.room.Database
 import androidx.room.RoomDatabase
+import androidx.room.TypeConverters
 
+@TypeConverters(Converters::class)
 @Database(entities = [Activity::class], version = 1)
 abstract class ActivityDatabase : RoomDatabase() {
     abstract fun activityDao(): ActivityDao
