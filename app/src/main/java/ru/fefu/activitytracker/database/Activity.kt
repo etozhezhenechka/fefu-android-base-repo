a8ea09aaf696b9e47205683f6bfba9f04de86ee1
Evgeny Shchukin <etozhezhenechka@outlook.com>
2021-11-12 13:42:37 +1000

    Add data entity for activity

---
+++ app/src/main/java/ru/fefu/activitytracker/database/Activity.kt
@@ -0,0 +1,16 @@
+package ru.fefu.activitytracker.database
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import ru.fefu.activitytracker.newactivity.model.ActivityType
+import java.time.LocalDateTime
+
+@Entity
+data class Activity(
+    @PrimaryKey(autoGenerate = true) val id: Int,
+    @ColumnInfo(name = "type") val type: ActivityType,
+    @ColumnInfo(name = "start_time") val startTime: LocalDateTime,
+    @ColumnInfo(name = "end_time") val endTime: LocalDateTime,
+    @ColumnInfo(name = "coordinate_list") val coordinateList: List<Pair<Double, Double>>
+)
