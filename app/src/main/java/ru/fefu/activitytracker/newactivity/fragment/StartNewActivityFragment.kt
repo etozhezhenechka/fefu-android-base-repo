3d226c6d56572c74681fe39c13dc2f1e228ece57
Evgeny Shchukin <etozhezhenechka@outlook.com>
2021-11-25 13:57:14 +1000

    Request location permission

--- app/src/main/java/ru/fefu/activitytracker/newactivity/fragment/StartNewActivityFragment.kt
+++ app/src/main/java/ru/fefu/activitytracker/newactivity/fragment/StartNewActivityFragment.kt
@@ -1,9 +1,17 @@
 package ru.fefu.activitytracker.newactivity.fragment
 
+import android.Manifest
+import android.app.AlertDialog
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.net.Uri
 import android.os.Bundle
+import android.provider.Settings
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.selection.SelectionTracker
 import androidx.recyclerview.selection.StableIdKeyProvider
@@ -11,6 +19,7 @@ import androidx.recyclerview.selection.StorageStrategy
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import ru.fefu.activitytracker.App
+import ru.fefu.activitytracker.BuildConfig
 import ru.fefu.activitytracker.R
 import ru.fefu.activitytracker.database.Activity
 import ru.fefu.activitytracker.databinding.FragmentStartNewActivityBinding
@@ -25,7 +34,22 @@ import java.time.LocalDateTime
 class StartNewActivityFragment : Fragment(R.layout.fragment_start_new_activity) {
     private var _binding: FragmentStartNewActivityBinding? = null
     private val binding get() = _binding!!
-    private lateinit var items: MutableList<ActivityTypeModel>
+    private lateinit var typeItems: MutableList<ActivityTypeModel>
+    private var locationPermissionGranted: Boolean = false
+
+    private val requestPermissionLauncher =
+        registerForActivityResult(ActivityResultContracts.RequestPermission()) { granted ->
+            if (granted) {
+                locationPermissionGranted = true
+            } else {
+                locationPermissionGranted = false
+                if (!shouldShowRequestPermissionRationale(Manifest.permission.ACCESS_FINE_LOCATION)) {
+                    showPermissionDeniedDialog()
+                } else {
+                    showRationaleDialog()
+                }
+            }
+        }
 
     companion object {
         const val tag = "start_new_activity_fragment"
@@ -52,9 +76,9 @@ class StartNewActivityFragment : Fragment(R.layout.fragment_start_new_activity) 
         val recycleView = binding.startActivityRecyclerView
         recycleView.layoutManager = LinearLayoutManager(activity, RecyclerView.HORIZONTAL, false)
 
-        fillList()
+        fillTypeList()
 
-        val typeAdapter = ActivityTypeAdapter(items)
+        val typeAdapter = ActivityTypeAdapter(typeItems)
         typeAdapter.setHasStableIds(true)
 
         recycleView.adapter = typeAdapter
@@ -70,19 +94,12 @@ class StartNewActivityFragment : Fragment(R.layout.fragment_start_new_activity) 
         (recycleView.adapter as ActivityTypeAdapter).tracker = selectionTracker
 
         binding.activityStartBtn.setOnClickListener {
-            ActivityForegroundService.startForeground(requireContext(), 111)
+            requestPermission()
 
-            App.INSTANCE.db.activityDao().insert(
-                Activity(
-                    0,
-                    ActivityType.values()[selectionTracker.selection.first().toInt()],
-                    LocalDateTime.of(2021, 1, 24, 12, 20),
-                    LocalDateTime.of(2021, 1, 24, 13, 41),
-                    listOf(Pair(1.0, 1.1), Pair(1.1, 1.0))
-                )
-            )
-
-            showNewFragment()
+            if (locationPermissionGranted) {
+                initProgressActivity(selectionTracker)
+                showNewFragment()
+            }
         }
     }
 
@@ -91,8 +108,8 @@ class StartNewActivityFragment : Fragment(R.layout.fragment_start_new_activity) 
         _binding = null
     }
 
-    private fun fillList() {
-        items = mutableListOf(
+    private fun fillTypeList() {
+        typeItems = mutableListOf(
             ActivityTypeModel(ActivityType.BIKING), ActivityTypeModel(ActivityType.RUNNING),
             ActivityTypeModel(ActivityType.SWIMMING)
         )
@@ -114,5 +131,63 @@ class StartNewActivityFragment : Fragment(R.layout.fragment_start_new_activity) 
 
             commit()
         }
+    }
+
+    private fun initProgressActivity(selectionTracker: SelectionTracker<Long>) {
+        ActivityForegroundService.startForeground(requireContext(), 111)
+
+        App.INSTANCE.db.activityDao().insert(
+            Activity(
+                0,
+                ActivityType.values()[selectionTracker.selection.first().toInt()],
+                LocalDateTime.of(2021, 1, 24, 12, 20),
+                LocalDateTime.of(2021, 1, 24, 13, 41),
+                listOf(Pair(1.0, 1.1), Pair(1.1, 1.0))
+            )
+        )
+    }
+
+    private fun requestPermission() {
+        when {
+            ContextCompat.checkSelfPermission(
+                requireContext(),
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ) == PackageManager.PERMISSION_GRANTED -> {
+                locationPermissionGranted = true
+            }
+
+            shouldShowRequestPermissionRationale(Manifest.permission.ACCESS_FINE_LOCATION) -> {
+                showRationaleDialog()
+            }
+
+            else -> {
+                requestPermissionLauncher.launch(Manifest.permission.ACCESS_FINE_LOCATION)
+            }
+        }
+    }
+
+    private fun showRationaleDialog() {
+        AlertDialog.Builder(requireContext())
+            .setTitle("Требуется разрешение")
+            .setMessage("Вы не можете отслеживать свою активность без местоположения")
+            .setPositiveButton("Запросить") { _, _ ->
+                requestPermissionLauncher.launch(Manifest.permission.ACCESS_FINE_LOCATION)
+            }
+            .setNegativeButton("Отмена") { _, _ -> }
+            .show()
+    }
+
+    private fun showPermissionDeniedDialog() {
+        AlertDialog.Builder(requireContext())
+            .setTitle("Требуется разрешение")
+            .setMessage("Пожалуйста, предоставьте разрешение в настройках приложения")
+            .setPositiveButton("Настройки") { _, _ ->
+                val intent = Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS)
+                val uri = Uri.fromParts("package", BuildConfig.APPLICATION_ID, null)
+                intent.data = uri
+                startActivity(intent)
+            }
+            .setNegativeButton("Отмена") { _, _ -> }
+            .show()
     }
 }
